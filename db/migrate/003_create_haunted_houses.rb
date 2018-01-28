b13eb35e405197a8e8dbed2303c056352b908aa9
Matt Trussell <smtrussell@gmail.com>
2018-01-28 22:34:53 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,5 +1,14 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration
   def change
+    create_table :haunted_houses do |t|
+      :name
+      :location
+      :theme
+      :price
+      :opening_date
+      :closing_date
+      :long_long_description
+    end
   end
 end
